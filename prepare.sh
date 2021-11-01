3c89df4e773a2f14e25e1ce1d00fc0c2fbd460f9
Sam Mousa <sam@mousa.nl>
2021-11-01 23:23:17 +0100

    ci: use whitelist for zip file inclusion

--- prepare.sh
+++ prepare.sh
@@ -3,10 +3,9 @@
 VERSION=$1
 pipx install gitman
 gitman install
-find libs -type d -name ".git" -execdir rm -rf {} \; -prune
 sed -i "s/{VERSION}/$VERSION/g" *.toc
 name=$(basename $(pwd))
 echo "Directory name is $name"
 cd ..
-zip -r $name/release.zip $name
+zip -r $name/release.zip $name --include \*.lua \*.xml \*.toc
 cd $name
