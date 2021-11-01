7b092eca65cfd4c42489042224ee7f2af1153f3d
Sam Mousa <sam@mousa.nl>
2021-11-01 23:07:29 +0100

    fix(ci): fix version replacement

--- prepare.sh
+++ prepare.sh
@@ -4,5 +4,5 @@ VERSION=$1
 pipx install gitman
 gitman install
 find libs -type d -name ".git" -execdir rm -rf {} \; -prune
-sed -i "s/{VERSION}/${{ steps.semantic.outputs.new_release_version }}/g" *.toc
+sed -i "s/{VERSION}/$VERSION/g" *.toc
 name=$(basename $(pwd)) pushd ..; zip -r $name/release.zip $name; popd
