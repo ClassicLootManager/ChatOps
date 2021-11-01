f65ebc8b958c29c38d11f6a2135317dff82095c6
Sam Mousa <sam@mousa.nl>
2021-11-01 23:15:35 +0100

    fix(ci): ue manual traversal, add pr workflow

--- prepare.sh
+++ prepare.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 VERSION=$1
 pipx install gitman
 gitman install
 find libs -type d -name ".git" -execdir rm -rf {} \; -prune
 sed -i "s/{VERSION}/$VERSION/g" *.toc
-name=$(basename $(pwd)) pushd ..; zip -r $name/release.zip $name; popd
+name=$(basename $(pwd))
+echo "Directory name is $name"
+cd ..
+zip -r $name/release.zip $name
+cd $name
